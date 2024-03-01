922cf9f6af8290777aa160a4c3eb319d67a6c1c4
Artur Ohanyan <arturo.yan10@gmail.com>
2024-03-01 19:08:45 +0400

    username

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <div className="page">
-      <header className="header">{TG?.initDataUnsafe?.user?.username ?? 'eee'}</header>
+      <header className="header">{TG?.initDataUnsafe?.user?.first_name}</header>
       <button onClick={onClose}>Close</button>
     </div>
   );
